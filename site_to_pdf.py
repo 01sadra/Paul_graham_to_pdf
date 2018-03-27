df1e3345a4cf2fb0e74932628d37993a523c2f5c
Sadra <01sadra@gmail.com>
2018-03-27 07:10:56 +0430

    Now we can get urls of archive page

--- site_to_pdf.py
+++ site_to_pdf.py
@@ -1,7 +1,18 @@
 import urllib2
 from bs4 import BeautifulSoup
-articles = "http://www.paulgraham.com/articles.html"
-page = urllib2.urlopen(articles)
-soup = BeautifulSoup(page,'html.parser')
-new_url=soup.find('a')
-print(new_url)
+
+def get_urls():
+    page = urllib2.urlopen("http://www.paulgraham.com/articles.html")
+    soup = BeautifulSoup(page,'html.parser')
+    urls = soup.findAll('table', {'width': '435'})[1].findAll('a')
+    articles = []
+    for url in urls:
+        print(url)
+        if  "http://" not in url["href"]: 
+            articles.append("http://www.paulgraham.com/" + url["href"]) 
+    return articles
+
+
+article_links = get_urls()
+for article_link in article_links:
+    print(article_link)
