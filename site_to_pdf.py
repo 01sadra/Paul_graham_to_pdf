533ed0edcf690bb5ff640ede0e3a34c81ad0173d
Sadra <01sadra@gmail.com>
2018-03-28 13:56:10 +0430

    Now everything works :)

--- site_to_pdf.py
+++ site_to_pdf.py
@@ -2,13 +2,9 @@ import urllib.request  as urllib2
 from bs4 import BeautifulSoup
 from fpdf import FPDF
 from fpdf import HTMLMixin
-import genshi
 class create_pdf(object):
-    def __init__(self, article_links, content_html, content_text):
-        self.article_links=article_links
-        self.content_html=content_html
-        self.content_text=content_text
 
+    # Getting the urls of articles in archive page and save them in an array
     def get_urls():
         page = urllib2.urlopen("http://www.paulgraham.com/articles.html")
         soup = BeautifulSoup(page,'html.parser')
@@ -19,33 +15,51 @@ class create_pdf(object):
                 articles.append("http://www.paulgraham.com/" + url["href"]) 
         return articles
 
+    # Getting the Article of given link
     def get_article(link):
-        page = urllib2.urlopen(link)
-        soup = BeautifulSoup(page,'html.parser')
-        font = str(soup.findAll('table', {'width':'435'})[0].findAll('font')[0])
-        if not 'Get funded by' in font and not 'Watch how this essay was' in font and not 'Like to build things?' in font and not len(font)<100:
-            content = font
-        else:
-            content = ''
-            for par in soup.findAll('table', {'width':'455'})[0].findAll('p'):
-                content += str(par)
-        return content
+        try:
+            page = urllib2.urlopen(link)
+            soup = BeautifulSoup(page,'html.parser')
+            font = str(soup.findAll('table', {'width':'435'})[0].findAll('font')[0])
+            if not 'Get funded by' in font and not 'Watch how this essay was' in font and not 'Like to build things?' in font and not len(font)<100:
+                content = font
+            else:
+                content = ''
+                for par in soup.findAll('table', {'width':'435'})[0].findAll('p'):
+                    content += str(par)
+            return content
+        except IndexError as erorr:
+            print(erorr)
 
-    def html_to_text(content):
-        return genshi.core.Markup(content.replace("\n","<br />").split("<br /><br />"))
+    #Eliminate the tags from given String and write it to the file
+    def add_to_pdf(text_content,pdf): 
+        try:
+            new_text = text_content \
+            .replace("<br />","\n") \
+            .replace("<br/>","\n") \
+            .replace("<br/><br/>","\n") \
+            .replace("<br /><br />","\n") \
+            .replace("<font face=\"verdana\" size=\"2\">", " ") \
+            .replace("</font>"," ") \
+            .replace("<b>"," ") \
+            .replace("<i>"," ") \
+            .replace("<ol>"," ") \
+        
+            pdf.add_page()
+            pdf.set_xy(0, 0)
+            pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
+            pdf.set_font('DejaVu', '', 14.0)
+            pdf.write(10, new_text)
+        except (UnicodeEncodeError , AttributeError) as erorr:
+            print(erorr)
 
-    def add_to_pdf(text_content):
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_xy(0, 0)
-        pdf.set_font('arial', 'B', 13.0)
-        pdf.cell(60, 10, "", 0, 1, 'C')
-        HTMLMixin.write_html(self = pdf,text = text_content, image_map = None)
-        pdf.output('paul.pdf', 'F')
 
-
+    pdf = FPDF()
     article_links = get_urls()
-    content_html = get_article(article_links[0])
-    content_text = html_to_text(content_html)
-    add_to_pdf(content_html)
-    
+    for link in article_links:
+        content_html = get_article(link)
+        add_to_pdf(content_html,pdf)    
+        print(link)
+
+    pdf.output('paul_graham.pdf', 'F')
+    print("done")
